7dc34c5758368c8c64533499ba7f0d2dee52d0c8
Takumis <Takumis@gmail.com>
2024-12-24 09:30:31 +0800

    semi-final feature

--- project2/api/consumers.py
+++ project2/api/consumers.py
@@ -7,16 +7,15 @@ logger = logging.getLogger(__name__)
 
 class TransactionConsumer(AsyncJsonWebsocketConsumer):
     async def connect(self):
-        logger.info(f"Websocket connect from client: {self.channel_name}")
-        logger.info(f"Attempting to add user to group: user_{self.scope['user'].id}")
-        await self.accept()
-        if self.scope["user"]:
+         logger.info(f"Websocket connect from client: {self.channel_name}")
+         await self.accept()
+         if self.scope["user"]:
             await self.channel_layer.group_add(f"user_{self.scope['user'].id}", self.channel_name)
-            logger.info(f"User {self.scope['user'].id} added to group")
+            logger.info(f"User {self.scope["user"]} added to the group ")
 
     async def disconnect(self, close_code):
-        logger.info(f"websocket disconnect from client: {self.channel_name}")
-        if self.scope["user"]:
+         logger.info(f"websocket disconnect from client: {self.channel_name}")
+         if self.scope["user"]:
             await self.channel_layer.group_discard(f"user_{self.scope['user'].id}", self.channel_name)
 
     async def send_notification(self,event):
