2fa063b74bdcbf4ffbc95d1a3126627181dc14ce
ayingleoneilivan <ayingleoneilivan@gmail.com>
2024-12-24 20:06:43 +0800

    Modified the transaction table to insert the money and receiver_account encrypted Revise the money and receiver_account to become textField Modified several functions, check for changes

--- project2/api/models.py
+++ project2/api/models.py
@@ -19,13 +19,13 @@ class BankAccount(models.Model):
 class Transaction(models.Model):
     def generate_transaction_id():
       return str(int(time.time()*1000))+ str(random.randint(1000,9999))
-    transaction_id = models.CharField(max_length=255, unique=True,editable=False,default=generate_transaction_id) # Add transaction id, and default function.
+    transaction_id = models.CharField(max_length=255, unique=True,editable=False,default=generate_transaction_id)
     account = models.ForeignKey(BankAccount, on_delete=models.CASCADE, related_name="sent_transaction")
-    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    amount = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     receiver = models.ForeignKey(User, on_delete=models.CASCADE, related_name="received_transaction")
-    receiver_account = models.CharField(max_length=100, null=True, blank=True)  # Add receiver account number
+    receiver_account = models.TextField()  # Add receiver account number
 
     def __str__(self):
-         return f"ID: {self.transaction_id}, Sender: {self.account.user}, Receiver: {self.receiver}, Amount: {self.amount}"
+        return f"ID: {self.transaction_id}, Sender: {self.account.user}, Receiver: {self.receiver}, Amount: {self.amount}"
